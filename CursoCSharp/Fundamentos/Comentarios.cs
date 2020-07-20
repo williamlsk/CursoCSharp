b0602ee2a2ee8fd2973af9ac64df644684c52689
William Leandro Silva <WILLIAM.SILVA@LAUREATE.COM.BR>
2020-07-20 12:34:21 -0300

    Adicionar arquivos de projeto.

---
+++ CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    public class Comentarios
+    {
+        public static void Executar()
+        {
+
+        }
+    }
+}
---
+++ CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    public class PrimeiroPrograma
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Primeiro ");
+            Console.WriteLine("Programa ");
+            Console.WriteLine("Terminou!");
+        }
+    }
+}
