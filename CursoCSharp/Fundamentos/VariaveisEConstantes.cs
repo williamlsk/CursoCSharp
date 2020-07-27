ac7073c7a379857dd4987c70b954a7f4966a0ec7
William Leandro Silva <WILLIAM.SILVA@LAUREATE.COM.BR>
2020-07-27 09:02:01 -0300

    Variáveis e Constantes

--- CursoCSharp/Fundamentos/VariaveisEConstantes.cs
+++ CursoCSharp/Fundamentos/VariaveisEConstantes.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml;
 
 namespace CursoCSharp.Fundamentos
 {
@@ -17,6 +18,52 @@ namespace CursoCSharp.Fundamentos
             double area = PI * raio * raio;
             Console.WriteLine(area);
             Console.WriteLine("Área é " + area);
+
+            //Tipos internos
+            bool estaChovendo = true;
+            Console.WriteLine("Está chovendo " + estaChovendo);
+
+            byte idade = 45;
+            Console.WriteLine("Idade : " + idade);
+
+            //pode haver valores negativos
+            sbyte saldoDeGols = sbyte.MinValue;
+            Console.WriteLine("Saldo de Gols : " + saldoDeGols);
+
+            short salario = short.MaxValue;
+            Console.WriteLine("Salario : " + salario );
+
+            //Mais usados dos inteiros
+            int menorValorInt = int.MinValue;
+            Console.WriteLine("Menor int :" + menorValorInt);
+
+            //inteiro sem sinal
+            uint populacaoBrasileira = 207_600_600;
+            Console.WriteLine("Populacao Brasileira: " + populacaoBrasileira);
+
+            //com sinal
+            long menorValorLong = long.MinValue;
+            Console.WriteLine("Menor valor Long: " + menorValorLong);
+
+            //sem sinal
+            ulong populacaoMundial = 7_600_000_000;
+            Console.WriteLine("População Mundial: " + populacaoMundial);
+
+            float precoComputador = 1299.99f;
+            Console.WriteLine("Preco Computador: " + precoComputador);
+
+            double valorDemercadoDaApple = 1_000_000_000_000.00; // mais usado
+            Console.WriteLine("Valor de mercado da Apple: " + valorDemercadoDaApple);
+
+            decimal distanciaEntreEstrelas = decimal.MaxValue;
+            Console.WriteLine("Distancia entre estrelas " + distanciaEntreEstrelas);
+
+            char letra = 'b';
+            Console.WriteLine("Letra: " + letra);
+
+            string texto = "Seja bem vindo ao curso de C#";
+            Console.WriteLine(texto);
+
         }
     }
 }
