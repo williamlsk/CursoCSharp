49ee107b21b04b5ad79e9cfc80cf3141b194d9b0
William Leandro Silva <WILLIAM.SILVA@LAUREATE.COM.BR>
2020-08-02 14:47:13 -0300

    Inferencia de Tipos

---
+++ CursoCSharp/Fundamentos/Inferencia.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class Inferencia
+    {
+        public static void Executar()
+        {
+            var nome = "William";
+            Console.WriteLine(nome);
+
+            //Quando houver um valor inferido, precisa inicializar com valor pro compilador identificar
+            var idade = 32;
+            Console.WriteLine(idade);
+
+            int a;
+            a = 3;
+
+            int b = 2;
+
+            Console.WriteLine(a+b);
+        }
+    }
+}
