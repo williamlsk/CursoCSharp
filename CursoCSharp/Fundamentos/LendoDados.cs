a3bc1c1c2ab895d2ef64eb92145bb47b3cf3e3e3
William Leandro Silva <WILLIAM.SILVA@LAUREATE.COM.BR>
2020-08-11 11:04:08 -0300

    Lendo dados no Console

---
+++ CursoCSharp/Fundamentos/LendoDados.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace CursoCSharp.Fundamentos
+{
+    public class LendoDados
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Qual é o seu nome? ");
+            string nome = Console.ReadLine();
+
+            //se o valor digitado não for uma string, precisa ser feito um cast, exemplo abaixo.
+            Console.WriteLine("Qual é a sua idade? ");
+            int idade = int.Parse(Console.ReadLine());
+
+            //Faz com que o salario separado por "," ou "."
+            Console.WriteLine("Qual é o seu salario? ");
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"{nome} {idade} R${salario}" );
+        }
+
+    }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -13,6 +13,7 @@ namespace CursoCSharp {
                 {"Inferencia - Fundamentos", Inferencia.Executar},
                 {"Interpolação - Fundamentos", Interpolacao.Executar},
                 {"Notação Ponto - Fundamentos", NotacaoPonto.Executar},
+                {"Lendo Dados - Fundamentos", LendoDados.Executar},
             });
 
             central.SelecionarEExecutar();
