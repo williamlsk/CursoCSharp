1b2280aa17c6685e35525e3ebffa654448b16cc0
William Leandro Silva <WILLIAM.SILVA@LAUREATE.COM.BR>
2020-09-20 21:43:18 -0300

    Operador ternario

---
+++ CursoCSharp/Fundamentos/OperadorTernario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class OperadorTernario
+    {
+        public static void Executar() 
+        {
+
+            var nota = 7.0;
+            bool bomComportamento = true;
+
+             //se a expressão for verdadeita, aprovado, se não, reprovado 
+            string resultato = nota >= 7.0 && bomComportamento ? "Aprovado" : "Reprovado";
+
+            Console.WriteLine(resultato);
+
+
+        }
+
+
+    }
+}
