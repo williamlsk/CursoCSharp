6ee7fbea3d326b4a5b266a6890ccca54f23051a6
William Leandro da Silva <william_lds@hotmail.com>
2020-11-22 23:36:46 -0300

    Colecoes - Array List

---
+++ CursoCSharp/Colecoes/ColecoesArrayList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace CursoCSharp.Colecoes
+{
+    class ColecoesArrayList
+    {
+        public static void Executar()
+        {
+            var arrayList = new ArrayList
+            {
+                "Palavra",
+                3,
+                true
+            };
+
+            arrayList.Add(3.14);
+            
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine("{0} => {1}", item, item.GetType());
+            }
+
+        }
+
+    }
+}
