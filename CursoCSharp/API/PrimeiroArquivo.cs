3a3ecba6e5abc21bff5fad2570a5ebb726a36628
William Leandro da Silva <william_lds@hotmail.com>
2021-01-17 15:44:11 -0300

    Explorando API - Escrevendo Arquivo

---
+++ CursoCSharp/API/PrimeiroArquivo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace CursoCSharp.API
+{
+
+    public static class ExtensaoString
+    {
+        public static string ParseHome(this string path)
+        {
+            string home = (Environment.OSVersion.Platform == PlatformID.Unix ||
+                  Environment.OSVersion.Platform == PlatformID.Win32NT)
+                  ? Environment.GetEnvironmentVariable("HOME")
+                  : Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+
+            return path.Replace("~", home);
+        }
+    }
+    class PrimeiroArquivo
+    {
+        public static void Executar()
+        {
+            //usa ~ para interpretar a home do usuário, no windows tem um comportamento linux e mac são outros
+            var path = @"C:/Projetos/primeiro_arquivo.txt";
+
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Esse é");
+                    sw.WriteLine("o nosso");
+                    sw.WriteLine("primeiro");
+                    sw.WriteLine("Arquivo!");
+                }
+            }
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine("");
+                sw.WriteLine("É possível");
+                sw.WriteLine("adicionar");
+                sw.WriteLine("mais texto");
+
+            }
+
+        }
+    }
+}
--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -8,6 +8,7 @@ using CursoCSharp.Colecoes;
 using CursoCSharp.OO;
 using CursoCSharp.MetodoEFuncoes;
 using CursoCSharp.Excecoes;
+using CursoCSharp.API;
 
 namespace CursoCSharp
 {
@@ -94,6 +95,10 @@ namespace CursoCSharp
                 //Exceções
                 {"Primeira Exceção - Exceções", PrimeiraExcecao.Executar},
                 {"Exceções Personalizadas - Exceções", ExcecoesPersonalizadas.Executar},
+
+                 //Exceções
+                {"Primeiro Arquivo - usando API", PrimeiroArquivo.Executar},
+
 
             });
 
